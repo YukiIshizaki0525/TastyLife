af1d33d6f153c911bf1f55f14a0d9f54aa8f686a
Yuki Ishizaki <ishiyuki405@icloud.com>
2020-07-04 16:41:27 +0900

    Eメールもしくはユーザー名入力でログインできるように変更しました。

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: ingredients
+#
+#  id         :bigint           not null, primary key
+#  content    :text(65535)      not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  recipe_id  :bigint           not null
+#
+# Indexes
+#
+#  index_ingredients_on_recipe_id  (recipe_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (recipe_id => recipes.id)
+#
 class Ingredient < ApplicationRecord
   belongs_to :recipe
 end
