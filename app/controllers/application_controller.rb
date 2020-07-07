964eec1a925154bef9f0515b5a19d836ddac4989
Yuki Ishizaki <ishiyuki405@icloud.com>
2020-07-07 22:38:31 +0900

    アイコン画像の追加・編集機能を追加しました。アイコン画像をGravatarからActiveStorageに変更いたしました。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,17 +8,16 @@ class ApplicationController < ActionController::Base
   # deviseコントローラーにストロングパラメータを追加する
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    root_path
+  end
 
   protected
     def configure_permitted_parameters
-      added_attrs = [:name, :email, :password, :password_confirmation, :remeber_me]
+      added_attrs = [:name, :email, :password, :password_confirmation, :remeber_me,:avatar]
       # 新規登録時のストロングパラメータ
       devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
       # アカウント編集時のストロングパラメータ
       devise_parameter_sanitizer.permit :account_update, keys: added_attrs
-    end
-
-    def after_sign_in_path_for(resource)
-      root_path
     end
 end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  
   protected
     # アカウント編集後、プロフィール画面に移動する
     def after_update_path_for(resource)
