fcaa2f31e96d68405477aeec339b904dd9f3cff2
Yuki Ishizaki <ishiyuki405@icloud.com>
2021-01-11 14:49:08 +0900

    レシピ・ユーザー・タイムライン一覧のデザイン変更を行いました。

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,8 +1,12 @@
 class HomesController < ApplicationController
   def index
     if user_signed_in?
-      @list = Recipe.page(params[:page])
+      @list = Recipe.page(params[:page]).limit(3)
       @tweet = current_user.feed.limit(3)
     end
+  end
+
+  def tweet_index
+    @tweet = current_user.feed
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show , :destroy, :following, :followers]
   def index
-    @users = User.page(params[:page]).per(10)
+    @users = User.page(params[:page]).per(6)
   end
 
   def show
