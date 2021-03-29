1a6437e7a253e6c6febc0e85e004a95579423421
Yuki Ishizaki <ishiyuki405@icloud.com>
2021-03-30 00:51:22 +0900

    login__guest-userクラスを指定して、ログインボタンと同じスタイルを適用しました。 RegistrationsControllerにensure_normal_userメソッド(ゲストユーザー削除不可)を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -138,5 +138,11 @@ class User < ApplicationRecord
     self.interests.exists?(consultation_id: consultation.id)
   end
 
-  
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
+      # 例えば name を入力必須としているならば， user.name = "ゲスト" なども必要
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,13 @@ Rails.application.routes.draw do
   
   # devise_for :users
   devise_for :users, controllers: {
-    registrations: 'users/registrations'
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
   }
+
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
 
   resources :recipes
   resources :users
