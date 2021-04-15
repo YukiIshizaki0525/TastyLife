dbb65f2d8a5ed9ba21f1d09c1556bf52f63f7b1f
Yuki Ishizaki <ishiyuki405@icloud.com>
2021-04-15 21:21:15 +0900

    タイムラインをトップページから削除し、ヘッダーとハンバーガーメニューから一覧表示できる仕様に変更

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,11 +3,9 @@ class HomesController < ApplicationController
     @recipes = Recipe.includes([:favorites], [user: { avatar_attachment: :blob }], [image_attachment: :blob]).page(params[:page]).limit(3)
 
     @consultations = Consultation.includes([:interests], [user: { avatar_attachment: :blob }]).page(params[:page]).per(3)
-    
-    @tweet = current_user.feed.limit(3)
   end
 
   def tweet_index
-    @tweet = current_user.feed.page(params[:page]).per(6)
+    @tweet = current_user.feed.includes([:favorites],  [user: { avatar_attachment: :blob }],[image_attachment: :blob]).page(params[:page]).per(6)
   end
 end
