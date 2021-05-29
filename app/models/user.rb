7643a61e06b89c46446e590edd92513ac3a170c0
Yuki Ishizaki <ishiyuki405@icloud.com>
2021-05-30 01:18:57 +0900

    現在のパスワードを入力せずにアカウント情報更新ができる仕様にしました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -167,4 +167,17 @@ class User < ApplicationRecord
       super
     end
   end
+
+  def update_without_current_password(params, *options)
+    params.delete(:current_password)
+
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+
+    result = update_attributes(params, *options)
+    clean_up_passwords
+    result
+  end
 end
