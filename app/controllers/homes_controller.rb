999a9d6b30a0360b15059c1eacd33bd30cc03f54
Yuki Ishizaki <ishiyuki405@icloud.com>
2021-06-05 14:21:30 +0900

    HomeControllerのActiveStorageのincludeの記述を削除 recipe_list.html.erbのimage表示の記述変更

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,10 +1,10 @@
 class HomesController < ApplicationController
   def index
-    @recipes = Recipe.includes([:favorites], [user: { avatar_attachment: :blob }], [image_attachment: :blob]).page(params[:page]).limit(3)
-    @consultations = Consultation.includes([:interests], [user: { avatar_attachment: :blob }]).page(params[:page]).per(3)
+    @recipes = Recipe.includes(:favorites).page(params[:page]).limit(3)
+    @consultations = Consultation.includes(:interests).page(params[:page]).per(3)
   end
 
   def tweet_index
-    @tweet = current_user.feed.includes([:favorites],  [user: { avatar_attachment: :blob }],[image_attachment: :blob]).page(params[:page]).per(6)
+    @tweet = current_user.feed.includes(:favorites).page(params[:page]).per(6)
   end
 end
