b1abf6406612408f0c68aeaedda92edba2ababa1
Yuki Ishizaki <ishiyuki405@icloud.com>
2021-06-12 18:44:45 +0900

    メールアドレス変更処理を実装しました。動作確認済みです。

--- app/mailers/users/mailer.rb
+++ app/mailers/users/mailer.rb
@@ -2,10 +2,13 @@ class Users::Mailer < Devise::Mailer
   helper :application
   include Devise::Controllers::UrlHelpers
   default template_path: 'devise/mailer'
-  
+
   def confirmation_instructions(record, token, opts={})
-    mail = super
-    mail.subject = "認証を行ってユーザ登録を完了してください"
-    mail
+    if record.unconfirmed_email != nil
+      opts[:subject] = "認証を行ってメールアドレス変更手続きを完了してください"
+    else
+      opts[:subject] = "認証を行ってユーザ登録を完了してください"
+    end
+    super
   end
 end
--- spec/system/user_edit_spec.rb
+++ spec/system/user_edit_spec.rb
@@ -2,4 +2,18 @@ require 'rails_helper'
 
 RSpec.describe "ユーザー編集", type: :system do
   let(:user) { create(:user) }
+  describe 'ユーザー編集' do
+    context 'フォームの入力値が正常' do
+      visit edit_user_registration_path
+      expect(page).to have_content('アカウント編集')
+      expect(page).to have_content, with: user.name
+      expect(page).to have_content, with: user.email
+
+      fill_in 'user_name', with: ''
+    end
+
+    context 'フォームの入力値が異常のため更新不可' do
+      
+    end
+  end
 end
