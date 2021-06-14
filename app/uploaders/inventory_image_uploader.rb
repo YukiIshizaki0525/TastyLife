d88c644e1f9ab6095264d3bc2066380dd6e7c5fb
Yuki Ishizaki <ishiyuki405@icloud.com>
2021-06-15 00:45:26 +0900

    デフォルト画像を全て変更

--- app/uploaders/inventory_image_uploader.rb
+++ app/uploaders/inventory_image_uploader.rb
@@ -19,6 +19,6 @@ class InventoryImageUploader < CarrierWave::Uploader::Base
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url(*args)
-    "no_image.png"
+    "no-image.png"
   end
 end
--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,1 +1,2 @@
 Time::DATE_FORMATS[:datetime_jp] = '%Y年%m月%d日'
+Time::DATE_FORMATS[:short] = "%m / %d %H:%M"
