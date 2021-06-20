247bacde0b990c67159f1fe756deb6fe5eca5272
Yuki Ishizaki <ishiyuki405@icloud.com>
2021-06-20 19:24:36 +0900

    Gemfile更新 不要な記述削除及び読み込み不要なものの排除

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
+  root 'homes#index'
   get 'consultation_interests/create'
   get 'consultation_interests/destroy'
-  root 'homes#index'
   get 'recipes/tweet', to: 'homes#tweet_index'
   get 'recipes/tag/:name', to: "recipes#tag_search"
   get 'recipes/search', to: 'recipes#search'
 
-  # letter_opner_webの設定
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-  
-  # devise_for :users
+  get 'users/:id/reregistration', to: 'users#reregistration', as: 'reregistration'
+  patch '/users/:id/withdrawal' => 'users#withdrawal', as: 'withdrawal'
+
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     passwords: 'users/passwords'
@@ -19,17 +18,7 @@ Rails.application.routes.draw do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
 
-  resources :recipes
   resources :users
-  resources :relationships, only: [:create, :destroy]
-
-  # 退会確認画面
-  # get '/users/:id/unsubscribe' => 'users#unsubscribe', as: 'unsubscribe'
-  # 論理削除用のルーティング
-  patch '/users/:id/withdrawal' => 'users#withdrawal', as: 'withdrawal'
-  
-  #再登録
-  get 'users/:id/reregistration', to: 'users#reregistration', as: 'reregistration'
 
   resources :users do
     member do
@@ -44,12 +33,14 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :relationships, only: [:create, :destroy]
+
+  resources :recipes
   resources :recipes do
     resources :comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
 
-  # 相談機能についてのルート
   resources :consultations
 
   resources :consultations do
@@ -57,6 +48,5 @@ Rails.application.routes.draw do
     resource :interests, only: [:create, :destroy]
   end
 
-  # 食材管理についてのルート
   resources :inventories, only: [:show, :new, :create, :edit, :update, :destroy]
 end
