afa2b6b41ec4eac77fad020886b48d1755dba877
Yuki Ishizaki <ishiyuki405@icloud.com>
2021-06-23 01:08:09 +0900

    スタイル調整

--- app/mailers/users/mailer.rb
+++ app/mailers/users/mailer.rb
@@ -2,7 +2,6 @@ class Users::Mailer < Devise::Mailer
   helper :application
   include Devise::Controllers::UrlHelpers
   default template_path: 'devise/mailer'
-  default from: '管理者'
 
   def confirmation_instructions(record, token, opts={})
     if record.unconfirmed_email != nil
