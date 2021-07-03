fd695ceb376d26c6f5614211b93e4c5eb467d718
Yuki Ishizaki <ishiyuki405@icloud.com>
2021-07-04 02:17:41 +0900

    変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,8 +8,7 @@ CarrierWave.configure do |config|
   if Rails.env.production?
     config.storage :fog
     config.fog_provider = 'fog/aws'
-    config.fog_directory  = 'recipeapp-s3'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/recipeapp-s3'
+    config.fog_directory = 'recipeapp-s3'
     config.fog_public = false
     config.fog_credentials = {
       provider: 'AWS',
