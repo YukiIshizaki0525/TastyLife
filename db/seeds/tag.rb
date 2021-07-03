529655cb6cbc130517fa68062e4634da274a2533
Yuki Ishizaki <ishiyuki405@icloud.com>
2021-07-03 16:42:37 +0900

    mysql8.0からmysql5.7へバージョン変更 本番環境用にdocker-compose変更

--- db/seeds/tag.rb
+++ db/seeds/tag.rb
@@ -1,10 +1,8 @@
-if Rails.env == 'development'
-  Tag.create([
-    { name: '栄養満点' },
-    { name: '簡単' },
-    { name: '時短' },
-    { name: 'コスパ◎' },
-    { name: '日持ち◎' },
-    { name: 'おもてなし' }
-  ])
-end
+Tag.first_or_create([
+  { name: '栄養満点' },
+  { name: '簡単' },
+  { name: '時短' },
+  { name: 'コスパ◎' },
+  { name: '日持ち◎' },
+  { name: 'おもてなし' }
+])
