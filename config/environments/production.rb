ae81ff95c200905c35702219cd295a46fb2563be
Yuki Ishizaki <ishiyuki405@icloud.com>
2021-07-04 00:29:16 +0900

    domainの行を削除

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,6 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:"smtp.gmail.com",
-    domain: 'gmail.com',
     port:587,
     user_name: ENV['SEND_MAIL'],
     password: ENV['GMAIL_SPECIFIC_PASSWORD'],
