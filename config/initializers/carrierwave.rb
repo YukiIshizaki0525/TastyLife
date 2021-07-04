94904f9cf0d5a13d6cc6f90d461c7fe222bfd141
Yuki Ishizaki <ishiyuki405@icloud.com>
2021-07-04 16:20:20 +0900

    RecipeAppからTastyLifeにロゴ変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -14,7 +14,7 @@ CarrierWave.configure do |config|
       provider: 'AWS',
       aws_access_key_id: ENV['S3_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
-      region: 'ap-northeast-1',
+      region: ENV['S3_REGION'],
       path_style: true
     }
   else
