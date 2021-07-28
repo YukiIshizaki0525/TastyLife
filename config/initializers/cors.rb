517700da3ced43971968b57fd72fcf63dccfc2cd
Yuki Ishizaki <ishiyuki405@icloud.com>
2021-07-28 21:00:59 +0900

    本番環境用pumaの設定追加 corsの設定追加

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000','https://tasty-life.site/'
+    origins 'localhost:3000', 'https://tasty-life.site/', 'd32nvik5ha78dd.cloudfront.net'
 
     resource "*",
       headers: :any,
