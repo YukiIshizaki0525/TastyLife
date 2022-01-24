40722f8660d69e6aa174afdf976054fff699e9e8
Yuki Ishizaki <ishiyuki405@icloud.com>
2022-01-25 01:40:45 +0900

    aws_secret_access_keyにカンマを追加

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -14,7 +14,7 @@ CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
       region: 'ap-northeast-1'
     }
     config.cache_storage = :fog
