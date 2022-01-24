2652890dfcda3991749cc6dfd60916fed5693ff3
Yuki Ishizaki <ishiyuki405@icloud.com>
2022-01-25 02:33:04 +0900

    S3で使用するバケット名とリージョン名を記述

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,13 +9,13 @@ CarrierWave.configure do |config|
     config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory = 'recipeapp-s3'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/recipeapp-s3'
 
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
       region: 'ap-northeast-1'
+      path_style: true
     }
     config.cache_storage = :fog
 
