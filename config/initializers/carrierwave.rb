3cd25a7262bf92dba173c11be1c9f04103af8052
Yuki Ishizaki <ishiyuki405@icloud.com>
2022-01-25 00:45:12 +0900

    アクセスキーを環境変数に設定

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,20 +6,22 @@ CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
 
 CarrierWave.configure do |config|
   if Rails.env.production?
+    config.storage :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_directory = 'recipeapp-s3'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/recipeapp-s3'
+
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: 'AKIAXVRFTZG676WAJLCS"',
-      aws_secret_access_key: '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',
-      region: 'ap-northeast-1'
+      region: 'ap-northeast-1',
+      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
     }
 
-    config.fog_directory = 'recipeapp-s3'
-    config.storage :fog
     config.cache_storage = :fog
 
   else
     config.storage :file
     config.enable_processing = false if Rails.env.test?
   end
-  
 end
