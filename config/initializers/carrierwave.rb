910ba9f4f47aae2f3f808b41950d9a6837d893d6
Yuki Ishizaki <ishiyuki405@icloud.com>
2022-01-25 01:15:11 +0900

    config.fog_credentialsの順番変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,16 +6,16 @@ CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
 
 CarrierWave.configure do |config|
   if Rails.env.production?
-    config.storage :fog
+    config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory = 'recipeapp-s3'
     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/recipeapp-s3'
 
     config.fog_credentials = {
       provider: 'AWS',
-      region: 'ap-northeast-1',
       aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
       aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
+      region: 'ap-northeast-1',
     }
 
     config.cache_storage = :fog
